aaf4569cb0418aca4add6caa6669c5c09a42de70
Motiejus Jakštys <1003704j@student.gla.ac.uk>
2012-02-16 13:36:17 +0000

    Initial Commit

---
+++ dpump/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  dpump
+//
+//  Created by Motiejus on 16/02/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
