690d10364a603cbbba4e512597724c05c5f7a4ba
Motiejus Jakštys <1003704j@student.gla.ac.uk>
2012-02-17 22:18:21 +0000

    Set up std err pipe in a separate NSThread (untested)

--- dpump/LogWatcher.m
+++ dpump/LogWatcher.m
@@ -14,14 +14,27 @@
 
 -(void)backgroundThread:(UITextView*)tv {    
     textView = tv;
-    [self performSelectorOnMainThread:@selector(addSomeText:)
-                           withObject:@"Labas"
-                        waitUntilDone:YES];
+    
+    int fd[2];
+    // data written to fd[1] appears on fd[0]
+    pipe(fd);
+    dup2(fd[1], 2); // stderr to our internal pipe
+    
+    char buf[4096];
+    int len;
+    while((len = read(fd[0], buf, 4095)) >= 0) {
+        buf[len] = '\0';
+        NSString *str = [[NSString alloc] initWithUTF8String:buf];
+
+        [self performSelectorOnMainThread:@selector(addSomeText:)
+                               withObject:str
+                            waitUntilDone:YES];
+    }
+    NSLog(@"Stopping LogWatcher");
 }
 
 -(void)addSomeText:(NSString *)text {
-    NSLog(@"Received: %@", text);
+    textView.text = [NSString stringWithFormat:@"%s %s", textView.text, text];
 }
-
 
 @end
